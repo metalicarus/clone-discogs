ca1356d4699d8538238038ed23a3956b51e61c22
Mateus Da Silva <silvamateus@brq.com>
2022-07-25 15:06:54 -0300

    first commit

---
+++ backend/discogs/src/main/java/br/com/hobgoblin/clone/discogs/DiscogsApplication.java
@@ -0,0 +1,13 @@
+package br.com.hobgoblin.clone.discogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscogsApplication.class, args);
+	}
+
+}
---
+++ backend/discogs/src/test/java/br/com/hobgoblin/clone/discogs/DiscogsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hobgoblin.clone.discogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
